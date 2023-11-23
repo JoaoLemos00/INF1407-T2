166a50ff976087cedef652230b051a8f97354b84
JoaoLemos00 <joao.lemos2703@gmail.com>
2023-11-23 15:53:36 -0300

    v1

--- backend/src/mysite/settings.py
+++ backend/src/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -168,3 +170,5 @@ TEMP = os.path.join(BASE_DIR, 'temp')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
